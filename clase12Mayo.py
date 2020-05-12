c28a54c2e79410b11d5d1242a48c1f43ea58682a
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2020-05-12 17:09:19 -0500

    Create clase12Mayo.py

---
+++ clase12Mayo.py
@@ -0,0 +1,21 @@
+if(45 < 34):
+    print("Verdadero")
+elif(99 < 1):
+    print("Si es mayor a 1")
+else:
+    print("no entro en ninguna")
+    
+def regresaNone():
+    print("No regreso nada")
+    
+def regresa5():
+    print("VA el 5")
+    return 5
+
+def sumaDatos(a,b):
+    suma = a + b
+    #print(suma)
+    return suma
+    
+    
+    
